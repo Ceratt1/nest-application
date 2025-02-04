88762d28b9187872c7b0dd6b1af72338cb1697b0
Ceratt1 <gabrielceratticabral@gmail.com.br>
2025-02-04 15:02:52 -0300

    swaggers ajusts

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,10 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsStrongPassword } from "class-validator";
 
 export class CreateUserDto {
-    
+    @ApiProperty({ example: 'user@example.com', description: 'Email do usuário' })
     @IsEmail()
     email: string;
     
+    @ApiProperty({ example: 'HashedPassword123', description: 'Senha criptografada do usuário' })
     @IsStrongPassword()
     password: string;
 }
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { user } from "@prisma/client";
 
 export class UserEntity implements user {
+    @ApiProperty({ example: 1, description: 'id do usuário' })
     id: number;
+
+    @ApiProperty({ example: 'user@example.com', description: 'Email do usuário' })
     email: string;
+
+    @ApiProperty({ example: 'HashedPassword123', description: 'Senha criptografada do usuário' })
     password: string;
 }
